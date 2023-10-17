f2b1f47904bcabfc89e4d24d16a9196e877bdabf
Raniel-Roque <raniel.roque0509@gmail.com>
2023-10-17 21:35:48 +0800

    Removed Unnecessary Code

--- LRT Ticket/TicketMain.vb
+++ LRT Ticket/TicketMain.vb
@@ -105,12 +105,4 @@
         Ticket_Out_Form.Show()
         Me.Close()
     End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-    End Sub
 End Class
